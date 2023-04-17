0fa51fb5a35b0a352fa0831d0dba8fbc9c99292c
alaaahmade <alaaahmade66@gmail.com>
2023-04-17 11:24:54 +0200

    Create add post fetch Relates #25

--- public/js/createpost/postValidation.js
+++ public/js/createpost/postValidation.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 const postValidation = (object) => {
   const { title, imageUrl, content } = object;
-  if (!title.match(/^[a-zA-Z]{3,100}$/)) {
+  // eslint-disable-next-line no-useless-escape
+  if (!(title.split(' ').join('')).match(/^[a-zA-Z!@#$&()-`.+,/\"]{3,100}$/)) {
     return 'Title It should not be more than 100 or less than a letter';
   } if (imageUrl) {
     if (imageUrl.length < 40) {
       return 'invalid image url';
     }
   }
-  return { title, imageUrl, content };
+  return true;
 };
