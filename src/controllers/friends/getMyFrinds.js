4233d39e41c2c299f5f710a01778fbceb8fa2da6
alaaahmade <alaaahmade66@gmail.com>
2023-04-23 03:32:48 +0200

    get my friendes from database Relate #55

---
+++ src/controllers/friends/getMyFrinds.js
@@ -0,0 +1,15 @@
+const { getMyFriendsQ } = require('../../database');
+
+const getMyFriends = (req, res, next) => {
+  const { userId } = req;
+  console.log(userId);
+  getMyFriendsQ(userId)
+    .then((data) => {
+      res.status(200).json({
+        error: false,
+        message: data.rows,
+      });
+    }).catch((error) => next(error));
+};
+
+module.exports = getMyFriends;
