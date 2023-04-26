a0e663e5e00a02241d1b0a6ef19ef58bbf2f3208
alaaahmade <alaaahmade66@gmail.com>
2023-04-26 19:51:44 +0200

    fix error

--- public/js/utils/helpers.js
+++ public/js/utils/helpers.js
@@ -31,13 +31,15 @@ sittingBtn.addEventListener('click', () => {
 
 const logoutBtn = document.getElementById('logout');
 
-logoutBtn.addEventListener('click', () => {
-  fetch('/user/logout', {
-    method: 'GET',
-  })
-    . then(() => {
-      window.location.href = '/page/index';
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', () => {
+    fetch('/user/logout', {
+      method: 'GET',
     })
-    // eslint-disable-next-line no-alert
-    .catch((error) => window.alert(error.message));
-});
+      . then(() => {
+        window.location.href = '/page/index';
+      })
+      // eslint-disable-next-line no-alert
+      .catch((error) => window.alert(error.message));
+  });
+}
